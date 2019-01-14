03527e5ebd98c641990febf32db6fbd7a98f4a21
Dawid <dawidmt@gmail.com>
2019-01-14 20:27:10 +0100

    cryptonose settings: -grouped settings using TitlesPanes -option to use default browser -list supported audio files extensions

--- src/main/java/com/dawidmotyka/cryptonose2/controllers/CryptonoseGuiSettingsController.java
+++ src/main/java/com/dawidmotyka/cryptonose2/controllers/CryptonoseGuiSettingsController.java
@@ -2,10 +2,14 @@ package com.dawidmotyka.cryptonose2.controllers;
 
 import com.dawidmotyka.cryptonose2.CryptonoseGuiSoundAlerts;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -13,17 +17,29 @@ import javax.sound.sampled.AudioFileFormat;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 import java.util.prefs.Preferences;
+import java.util.stream.Collectors;
 
 /**
  * Created by dawid on 8/8/17.
  */
 public class CryptonoseGuiSettingsController implements Initializable{
 
+    @FXML
     public TextField browserPathEditText;
+    @FXML
     public TextField priceRisingSoundFileEditText;
+    @FXML
     public TextField priceFallingSoundFileEditText;
+    @FXML
+    public HBox browserPathHBox;
+    @FXML
+    public Text supportedAudioFilesText;
+    @FXML
+    public CheckBox defBrowserCheckbox;
+
     private Preferences preferences;
 
     @Override
@@ -32,6 +48,10 @@ public class CryptonoseGuiSettingsController implements Initializable{
         browserPathEditText.setText(preferences.get("browserPath", ""));
         priceRisingSoundFileEditText.setText(preferences.get("soundRisingPath", ""));
         priceFallingSoundFileEditText.setText(preferences.get("soundDroppingPath", ""));
+        defBrowserCheckbox.setSelected(preferences.getBoolean("tryUseDefBrowser",true));
+        browserPathHBox.setDisable(defBrowserCheckbox.isSelected());
+        supportedAudioFilesText.textProperty().setValue(supportedAudioFilesText.getText()
+                +Arrays.stream(CryptonoseGuiSoundAlerts.getAudioFileTypes()).map(type -> String.format("%s (*.%s)",type.toString(), type.getExtension())).collect(Collectors.joining(", ")));
     }
 
     public void selectBrowserClick(ActionEvent actionEvent) {
@@ -98,5 +118,12 @@ public class CryptonoseGuiSettingsController implements Initializable{
         Button button = (Button) actionEvent.getSource();
         Stage stage = (Stage) button.getScene().getWindow();
         stage.close();
+    }
+
+    public void defBrowserCheckboxOnAction(ActionEvent actionEvent) {
+        if(((CheckBox)actionEvent.getSource()).isSelected())
+            browserPathHBox.setDisable(true);
+        else
+            browserPathHBox.setDisable(false);
     }
 }
