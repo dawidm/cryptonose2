278b2309372c0aa24a55dc3da9ade6b60996ab20
Dawid <dawidmt@gmail.com>
2019-01-28 15:38:37 +0100

    remove Twinkle

--- src/main/java/com/dawidmotyka/cryptonose2/CryptonoseGuiNotification.java
+++ src/main/java/com/dawidmotyka/cryptonose2/CryptonoseGuiNotification.java
@@ -1,11 +1,5 @@
 package com.dawidmotyka.cryptonose2;
 
-import ch.swingfx.twinkle.NotificationBuilder;
-import ch.swingfx.twinkle.event.NotificationEvent;
-import ch.swingfx.twinkle.event.NotificationEventAdapter;
-import ch.swingfx.twinkle.style.INotificationStyle;
-import ch.swingfx.twinkle.style.theme.DarkDefaultNotification;
-import ch.swingfx.twinkle.window.Positions;
 import com.dawidmotyka.exchangeutils.exchangespecs.ExchangeSpecs;
 import dorkbox.notify.Notify;
 import javafx.util.Duration;
@@ -18,7 +12,7 @@ public class CryptonoseGuiNotification {
     public static final int HIDE_AFTER=7000;
     public static final DecimalFormat PRICE_DECIMAL_FORMAT=new DecimalFormat("#.########");
 
-    public enum NotificationLibrary {CONTROLSFX,DORKBOX,TWINKLE};
+    public enum NotificationLibrary {CONTROLSFX,DORKBOX};
 
     public static void notifyPriceAlert(NotificationLibrary notificationLibrary, PriceAlert priceAlert, Runnable action) {
         String notifyText = String.format("%.2f%% (rel %.2f), %s (%ds), final price: %s",
@@ -35,9 +29,6 @@ public class CryptonoseGuiNotification {
             case DORKBOX:
                 notifyDorkbox(notifyTitle,notifyText,action);
                 break;
-            case TWINKLE:
-                notifyTwinkle(notifyTitle,notifyText,action);
-                break;
         }
     }
 
@@ -48,9 +39,6 @@ public class CryptonoseGuiNotification {
                 break;
             case DORKBOX:
                 notifyDorkbox(exchangeSpecs.getName(),cryptonoseGuiConnectionStatus.getText(),null);
-                break;
-            case TWINKLE:
-                notifyTwinkle(exchangeSpecs.getName(),cryptonoseGuiConnectionStatus.getText(),null);
                 break;
         }
     }
@@ -66,7 +54,6 @@ public class CryptonoseGuiNotification {
             if(action!=null)
                 notifications.onAction((actionEvent) -> action.run());
             notifications.show();
-
         });
     }
 
@@ -75,21 +62,5 @@ public class CryptonoseGuiNotification {
         if(action!=null)
             notify=notify.onAction((notify1 -> action.run()));
         notify.show();
-    }
-
-    private static void notifyTwinkle(String title, String text, Runnable action) {
-        INotificationStyle style = new DarkDefaultNotification()
-                .withWidth(400) // Optional
-                .withAlpha(0.9f); // Optional
-        new NotificationBuilder()
-                .withStyle(style) // Required. here we set the previously set style
-                .withTitle(title) // Required.
-                .withMessage(text) // Optional
-                .withDisplayTime(10000) // Optional
-                .withPosition(Positions.SOUTH_EAST) // Optional. Show it at the center of the screen
-                .withListener(new NotificationEventAdapter() { // Optional
-                    public void clicked(NotificationEvent event) {action.run();}
-                })
-                .showNotification(); // this returns a UUID that you can use to identify events on the listener
     }
 }
