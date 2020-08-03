b9d063cd85c766829c2fdc75c1d8f49ada6ac26a
Dawid Motyka <dawidmt@gmail.com>
2020-08-03 23:35:50 +0200

    add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module cryptonose {
+    requires javafx.controls;
+    requires java.desktop;
+    requires java.logging;
+    requires java.prefs;
+    requires exchangeutils;
+    requires cryptonoseengine;
+    requires javafx.fxml;
+    requires xchange.core;
+    requires Notify;
+    exports pl.dmotyka.cryptonose2.controllers;
+}
