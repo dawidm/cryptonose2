3c9283d93f4e458fbaac4e15be71a89db14e5429
Dawid Motyka <dawidmt@gmail.com>
2020-08-08 20:50:57 +0200

    alerts settings: add exchange name to save button, change buttons layout

--- src/main/java/pl/dmotyka/cryptonose2/controllers/CryptonoseGuiAlertSettingsController.java
+++ src/main/java/pl/dmotyka/cryptonose2/controllers/CryptonoseGuiAlertSettingsController.java
@@ -1,7 +1,7 @@
 /*
  * Cryptonose2
  *
- * Copyright © 2019 Dawid Motyka
+ * Copyright © 2019-2020 Dawid Motyka
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
@@ -21,6 +21,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -73,6 +74,8 @@ public class CryptonoseGuiAlertSettingsController implements Initializable {
     public Label p2titleLabel;
     @FXML
     public TitledPane p2periodSettingsTitledPane;
+    @FXML
+    public Button saveButton;
 
     private long[] timePeriods;
 
@@ -146,6 +149,7 @@ public class CryptonoseGuiAlertSettingsController implements Initializable {
     }
 
     public void init(ExchangeSpecs exchangeSpecs, long[] timePeriods) {
+        saveButton.setText(saveButton.getText() + String.format(" for %s", exchangeSpecs.getName()));
         if(timePeriods.length<2) {
             throw new IllegalArgumentException("timePeriods array should have at least 2 elements");
         }
