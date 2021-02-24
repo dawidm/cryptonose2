7b1155dd51e9cf43236a2c4db4acdf855e02e536
Dawid Motyka <dawidmt@gmail.com>
2021-02-24 12:41:13 +0100

    increase number of recent alerts on alerts tab

--- src/main/java/pl/dmotyka/cryptonose2/controllers/CryptonoseGuiPriceAlertsTabController.java
+++ src/main/java/pl/dmotyka/cryptonose2/controllers/CryptonoseGuiPriceAlertsTabController.java
@@ -1,7 +1,7 @@
 /*
  * Cryptonose
  *
- * Copyright © 2019-2020 Dawid Motyka
+ * Copyright © 2019-2021 Dawid Motyka
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
@@ -35,7 +35,7 @@ public class CryptonoseGuiPriceAlertsTabController implements Initializable{
     @FXML
     public VBox mainVBox;
 
-    private int maxAlerts = 30;
+    private int maxAlerts = 100;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
