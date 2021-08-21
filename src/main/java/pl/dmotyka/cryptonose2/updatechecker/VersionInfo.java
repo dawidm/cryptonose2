ba678cd48cd5e99305e3dc9f831f2e6c5e1f9835
Dawid Motyka <dawidmt@gmail.com>
2021-08-21 14:10:48 +0200

    add release description to VersionInfo

--- src/main/java/pl/dmotyka/cryptonose2/updatechecker/VersionInfo.java
+++ src/main/java/pl/dmotyka/cryptonose2/updatechecker/VersionInfo.java
@@ -1,7 +1,7 @@
 /*
  * Cryptonose
  *
- * Copyright © 2019-2020 Dawid Motyka
+ * Copyright © 2019-2021 Dawid Motyka
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
@@ -17,10 +17,12 @@ public class VersionInfo {
 
     private String versionString;
     private String downloadUrl;
+    private String description;
 
-    public VersionInfo(String versionString, String downloadUrl) {
+    public VersionInfo(String versionString, String downloadUrl, String description) {
         this.versionString = versionString;
         this.downloadUrl = downloadUrl;
+        this.description = description;
     }
 
     public String getVersionString() {
