b5ae8eeefc2ff3ef00b21ac63383dba19896632f
Dawid Motyka <dawidmt@gmail.com>
2022-01-12 18:46:09 +0100

    fix coin plugins symbols for uniswap polygon

--- src/main/java/pl/dmotyka/cryptonose2/tools/CoinPluginConverter.java
+++ src/main/java/pl/dmotyka/cryptonose2/tools/CoinPluginConverter.java
@@ -1,7 +1,7 @@
 /*
  * Cryptonose
  *
- * Copyright © 2019-2021 Dawid Motyka
+ * Copyright © 2019-2022 Dawid Motyka
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
@@ -18,12 +18,13 @@ import java.util.Locale;
 import pl.dmotyka.exchangeutils.exchangespecs.ExchangeSpecs;
 import pl.dmotyka.exchangeutils.thegraphuniswapv3.Uniswap3ExchangeSpecs;
 import pl.dmotyka.exchangeutils.thegraphuniswapv3.Uniswap3PairDataProvider;
+import pl.dmotyka.exchangeutils.thegraphuniswapv3.Uniswap3PolygonExchangeSpecs;
 
 public class CoinPluginConverter {
 
     public static String apiSymbolToPluginSymbol(String apiSymbol, ExchangeSpecs exchangeSpecs) {
         String baseCurrency = exchangeSpecs.getPairSymbolConverter().apiSymbolToXchangeCurrencyPair(apiSymbol).base.getCurrencyCode();
-        if (exchangeSpecs instanceof Uniswap3ExchangeSpecs) {
+        if (exchangeSpecs instanceof Uniswap3ExchangeSpecs || exchangeSpecs instanceof Uniswap3PolygonExchangeSpecs) {
             baseCurrency = ((Uniswap3PairDataProvider)exchangeSpecs.getPairDataProvider()).getTokenInfo(baseCurrency.toLowerCase(Locale.ROOT)).getTokenSymbol();
         }
         return baseCurrency;
